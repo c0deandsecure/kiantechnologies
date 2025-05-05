206b90323a464bf8e8b9a90cd1e426cdbafb09d4
Karandeep Singh <130697957+Chakarvartit@users.noreply.github.com>
2025-05-05 12:00:30 +0530

    ECCouncil Add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Courses from './components/Courses';
 import Contact from './components/Contact';
 import Internship from './components/Intership';
 import BestEthical from './components/BestEthical';
+import ECCouncil from './components/ECCouncil';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ECCouncil />
       <Contact />
       <Courses />
       <BestEthical />
