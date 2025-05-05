e3956b7a369c1359c6d970e2deb288d94becbee9
Karandeep Singh <130697957+Chakarvartit@users.noreply.github.com>
2025-05-05 12:17:50 +0530

    LatestSection Add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/Contact';
 import Internship from './components/Intership';
 import BestEthical from './components/BestEthical';
 import ECCouncil from './components/ECCouncil';
+import LatestSection from './components/LatestSection';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Courses />
       <BestEthical />
       <Internship />
+      <LatestSection />
     </div>
   );
 }
