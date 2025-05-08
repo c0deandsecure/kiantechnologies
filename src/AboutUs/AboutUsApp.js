d51fa181a205d98d1cf9f5daea071c0dc01afa13
Karandeep Singh <130697957+Chakarvartit@users.noreply.github.com>
2025-05-08 13:56:14 +0530

    about page almost done

--- src/AboutUs/AboutUsApp.js
+++ src/AboutUs/AboutUsApp.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Footer from "../home/Footer";
 import AboutHero from "./AboutHero";
+import InternshipSection from "./InternshipSection";
+import AboutWithNews from "./AboutWithNews";
+
 const AboutUsApp = () => {
   return (
     <>
-      
       <AboutHero />
-     
+      <InternshipSection />
+      <AboutWithNews />
+
       <Footer />
     </>
   );
