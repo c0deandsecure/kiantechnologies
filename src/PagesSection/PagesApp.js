7c7b0208ebeb8aef1987a1257fab2ca09dfd2fe2
Karandeep Singh <130697957+Chakarvartit@users.noreply.github.com>
2025-06-12 01:52:31 +0530

    page secton were updated

--- src/PagesSection/PagesApp.js
+++ src/PagesSection/PagesApp.js
@@ -1,20 +1,24 @@
 // src/Pages/PagesApp.js
 import React from 'react';
+import PagesHero from './pagesHero';
 import Teacher from './Teacher';
 import Blog from './Blog';
 import FacultyDetails from './FacultyDetails';
 import CaseStudies from './CaseStudies';
-import Masterclass from './Masterclass';
+
+import Contact from '../home/Contact';
 import './Pages.css';
 
 const PagesApp = () => {
   return (
     <div className="pages-container">
+      <PagesHero />
+      {/* The following components can be rendered conditionally based on the route or user interaction */}
       <Teacher />
       <Blog />
       <FacultyDetails />
       <CaseStudies />
-      <Masterclass />
+      <Contact />
     </div>
   );
 };
